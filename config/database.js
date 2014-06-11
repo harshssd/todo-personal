7a91134019389b3c09c614daf02a553503f45d59
harsha <harsha@harsha-Lenovo-U410.(none)>
2014-06-11 15:32:12 -0400

    onmodulus mongo

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
 
 	// the database url to connect
-	url : 'mongodb://node:node@mongo.onmodulus.net:27017/uwO3mypu'
+	url : 'mongodb://root:root@novus.modulusmongo.net:27017/Sope5him'
 }
