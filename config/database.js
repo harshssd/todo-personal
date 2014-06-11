a65cb02a36d6f6aed7ae54e85aa52d00650a0cf9
harsha <harsha@harsha-Lenovo-U410.(none)>
2014-06-11 15:20:10 -0400

    back to original on modulus

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
 
 	// the database url to connect
-	url : 'mongodb://harsha.hassansomashekara@cengage.com:<Harsha123>@novus.modulusmongo.net:27017/ig6oGuty'
+	url : 'mongodb://node:node@mongo.onmodulus.net:27017/uwO3mypu'
 }
